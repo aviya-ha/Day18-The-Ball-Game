33ddcd67100044c9b70dc6e0c8b18a7260703ff4
aviya-ha <aviyaharel6@gmail.com>
2024-02-13 17:46:45 +0200

    Change onBallClick fun to increment the balls width and height by 50px

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 'use strict'
+var gCurrSize = 100
 
-function onBallClick(){
-    console.log('Hey');
+function onBallClick() {
+    var addingSize = 50
+    const elBall = document.querySelector('.ball')
+    elBall.style.width = gCurrSize + addingSize + 'px'
+    elBall.style.height = gCurrSize + addingSize + 'px'
+    gCurrSize += addingSize
+    elBall.innerText = gCurrSize
+
 }
