a65ae1857d31b9a0cc80a185810b8b63d1ba6486
aviya-ha <aviyaharel6@gmail.com>
2024-02-13 20:14:10 +0200

    Add getRandomColor() to ball

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function onBallClick() {
     const addingSize = getRandomInt(20,60)
     const elBall = document.querySelector('.ball')
     const elBallSize = document.querySelector('.ball-size')
+    elBall.style.backgroundColor = getRandomColor()
     if(gCurrSize < 400){
         elBall.style.width = gCurrSize + addingSize + 'px'
         elBall.style.height = gCurrSize + addingSize + 'px'
--- js/util.js
+++ js/util.js
@@ -3,8 +3,16 @@
 function getRandomInt(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)+1) + min
+    return Math.floor(Math.random() * (max - min) + 1) + min
 }
 
 
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
