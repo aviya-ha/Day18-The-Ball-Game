babd8f93a7baec0a9d4ee24b66236561a46a5735
aviya-ha <aviyaharel6@gmail.com>
2024-02-13 23:03:23 +0200

    Make both balls to change onclick  and add maxDiameter to onBallClick

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 'use strict'
-var gCurrSize = 100
+var gCurrSizeBall = 100
+var gCurrSizeBall2 = 100
 
-function onBallClick() {
-    const addingSize = getRandomInt(20,60)
-    const elBall = document.querySelector('.ball')
-    const elBallSize = document.querySelector('.ball-size')
+const maxDiameter = 400
+
+
+function onBallClick(ball) {
+    var CurrSize
+    const addingSize = getRandomInt(20, 60)
+    const currBallClass = '.' + ball.classList[0]
+    const elBall = document.querySelector(currBallClass)
+    const elBallSize = document.querySelector(currBallClass, 'span')
     elBall.style.backgroundColor = getRandomColor()
-    if(gCurrSize < 400){
-        elBall.style.width = gCurrSize + addingSize + 'px'
-        elBall.style.height = gCurrSize + addingSize + 'px'
-        gCurrSize += addingSize
-        elBallSize.innerText = gCurrSize
-    }else{
-        gCurrSize = 100
-        elBall.style.width = gCurrSize + 'px'
-        elBall.style.height = gCurrSize + 'px'
-        elBallSize.innerText = gCurrSize
+    if (currBallClass === '.ball') { CurrSize = gCurrSizeBall 
+    }else{CurrSize = gCurrSizeBall2} 
+    
+    if (CurrSize + addingSize > maxDiameter) {
+        CurrSize = 100
+        elBall.style.width = CurrSize + 'px'
+        elBall.style.height = CurrSize + 'px'
+        elBallSize.innerText = CurrSize
+    } else {
+        elBall.style.width = CurrSize + addingSize + 'px'
+        elBall.style.height = CurrSize + addingSize + 'px'
+        CurrSize += addingSize
+        elBallSize.innerText = CurrSize
     }
-
+    if (currBallClass === '.ball') {  gCurrSizeBall =  CurrSize
+    }else{ gCurrSizeBall2 = CurrSize} 
 }
