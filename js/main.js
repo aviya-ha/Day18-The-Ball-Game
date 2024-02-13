8a0f5dc5036535d15b7c1686404c6cc71fc0b3e1
aviya-ha <aviyaharel6@gmail.com>
2024-02-13 19:29:09 +0200

    Add max size 400 to ball and r size it after

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ var gCurrSize = 100
 function onBallClick() {
     const addingSize = 50
     const elBall = document.querySelector('.ball')
-    elBall.style.width = gCurrSize + addingSize + 'px'
-    elBall.style.height = gCurrSize + addingSize + 'px'
     const elBallSize = document.querySelector('.ball-size')
-    gCurrSize += addingSize
-    elBallSize.innerText = gCurrSize
+    if(gCurrSize < 400){
+        elBall.style.width = gCurrSize + addingSize + 'px'
+        elBall.style.height = gCurrSize + addingSize + 'px'
+        gCurrSize += addingSize
+        elBallSize.innerText = gCurrSize
+    }else{
+        gCurrSize = 100
+        elBall.style.width = gCurrSize + 'px'
+        elBall.style.height = gCurrSize + 'px'
+        elBallSize.innerText = gCurrSize
+    }
 
 }
