c368616e82c5461e9d5bf2fc16c49df22a47d55f
aviya-ha <aviyaharel6@gmail.com>
2024-02-14 04:01:23 +0200

    Rearrangement js main fun

--- js/main.js
+++ js/main.js
@@ -4,97 +4,116 @@ var gCurrColorBall = 'rgb(255, 0, 119)'
 var gCurrSizeBall2 = 100
 var gCurrColorBall2 = 'rgb(237, 173, 70)'
 var gIsBallThreeClicked = false
-
-
-
+var gCurrSize
 const maxDiameter = 400
 
 
-function onBallClick(ball,) {
-    var CurrSize
-    const addingSize = getRandomInt(20, 60)
+function onBallClick(ball) {
+    var addingSize = getRandomInt(20, 60)
     const currBallClass = '.' + ball.classList[0]
     const elBall = document.querySelector(currBallClass)
-    const elBallSize = document.querySelector(currBallClass, 'span')
     if (currBallClass === '.ball') {
-        gCurrColorBall = getRandomColor()
-        elBall.style.backgroundColor = gCurrColorBall
-        CurrSize = gCurrSizeBall
+        gCurrSize = gCurrSizeBall
     } else {
-        gCurrColorBall2 = getRandomColor()
-        elBall.style.backgroundColor = gCurrColorBall2
-        CurrSize = gCurrSizeBall2
+        gCurrSize = gCurrSizeBall2
+        console.log('gCurrSize:', gCurrSize)
     }
-
-    if (CurrSize + addingSize > maxDiameter) {
-        CurrSize = 100
-        elBall.style.width = CurrSize + 'px'
-        elBall.style.height = CurrSize + 'px'
-        elBallSize.innerText = CurrSize
-    } else {
-        elBall.style.width = CurrSize + addingSize + 'px'
-        elBall.style.height = CurrSize + addingSize + 'px'
-        CurrSize += addingSize
-        elBallSize.innerText = CurrSize
-    }
+    changeColor(elBall,currBallClass)
+    changeSize(elBall,addingSize)
+    changeInnerText(elBall, addingSize)
+    console.log('gCurrSize:', gCurrSize)
+    
     if (currBallClass === '.ball') {
-        gCurrSizeBall = CurrSize
+        gCurrSizeBall = gCurrSize
     } else {
-        gCurrSizeBall2 = CurrSize
+        gCurrSizeBall2 = gCurrSize
     }
 }
 
 function OnThirdBall() {
     const elBall = document.querySelector('.ball')
     const elBall2 = document.querySelector('.ball2')
-
+    
     var temBallSize = gCurrSizeBall
     var temBall2Size = gCurrSizeBall2
-    switchInnerText()
-    
-    if (!gIsBallThreeClicked) {
-        elBall.style.backgroundColor = gCurrColorBall2
-        elBall2.style.backgroundColor = gCurrColorBall
 
-        
-        elBall.style.width = temBall2Size + 'px'
-        elBall.style.height = temBall2Size + 'px'
+    switchInnerText(elBall, elBall2, temBallSize, temBall2Size)
+    switchSize(elBall, elBall2, temBallSize, temBall2Size)
+    switchColor(elBall, elBall2)
+    if (!gIsBallThreeClicked) gIsBallThreeClicked = true
+    else gIsBallThreeClicked = false
+}
 
-       
-        elBall2.style.width = temBallSize + 'px'
-        elBall2.style.height = temBallSize + 'px'
-
-        gIsBallThreeClicked = true
+function changeColor(elBall,currBallClass) {
+    if (currBallClass === '.ball') {
+        gCurrColorBall = getRandomColor()
+        elBall.style.backgroundColor = gCurrColorBall
 
     } else {
-        elBall.style.backgroundColor = gCurrColorBall
-        elBall2.style.backgroundColor = gCurrColorBall2
-
-        
-        elBall.style.width = temBallSize + 'px'
-        elBall.style.height = temBallSize + 'px'
-
-        
-        elBall2.style.width = temBall2Size + 'px'
-        elBall2.style.height = temBall2Size + 'px'
-
-        gIsBallThreeClicked = false
+        gCurrColorBall2 = getRandomColor()
+        elBall.style.backgroundColor = gCurrColorBall2
 
     }
 }
 
+function changeSize(elBall, addingSize) {
+    console.log('elBall:', elBall)
+    if (gCurrSize + addingSize > maxDiameter) {
+        gCurrSize = 100
+        elBall.style.width = gCurrSize + 'px'
+        elBall.style.height = gCurrSize + 'px'
+    } else {
+        console.log('gCurrSize:', gCurrSize)
+        elBall.style.width = gCurrSize + addingSize + 'px'
+        elBall.style.height = gCurrSize + addingSize + 'px'
+        gCurrSize += addingSize
+    }
+}
 
-function switchInnerText() {
-    const elBall = document.querySelector('.ball') 
-    const elBall2 = document.querySelector('.ball2')
-  
-    var temBallSize = gCurrSizeBall
-    var temBall2Size = gCurrSizeBall2
+function changeInnerText(elBall,addingSize) {
+    if (gCurrSize + addingSize > maxDiameter) {
+        console.log('gCurrSize:', gCurrSize)
+        elBall.innerText = gCurrSize
+    } else {
+        console.log('gCurrSize:', gCurrSize)
+        elBall.innerText = gCurrSize
+    }
+}
+
+function switchInnerText(elBall, elBall2, temBallSize, temBall2Size) {
+
     if (!gIsBallThreeClicked) {
         elBall.innerText = temBall2Size
         elBall2.innerText = temBallSize
     } else {
         elBall.innerText = temBallSize
         elBall2.innerText = temBall2Size
+    }
+}
+
+function switchSize(elBall, elBall2, temBallSize, temBall2Size) {
+    if (!gIsBallThreeClicked) {
+        elBall.style.width = temBall2Size + 'px'
+        elBall.style.height = temBall2Size + 'px'
+
+        elBall2.style.width = temBallSize + 'px'
+        elBall2.style.height = temBallSize + 'px'
+    } else {
+        elBall.style.width = temBallSize + 'px'
+        elBall.style.height = temBallSize + 'px'
+
+        elBall2.style.width = temBall2Size + 'px'
+        elBall2.style.height = temBall2Size + 'px'
+    }
+}
+
+function switchColor(elBall, elBall2) {
+    if (!gIsBallThreeClicked) {
+        elBall.style.backgroundColor = gCurrColorBall2
+        elBall2.style.backgroundColor = gCurrColorBall
+
+    } else {
+        elBall.style.backgroundColor = gCurrColorBall
+        elBall2.style.backgroundColor = gCurrColorBall2
     }
 }
--- js/util.js
+++ js/util.js
@@ -17,3 +17,40 @@ function getRandomColor() {
     return color
 }
 
+function switchInnerText(elBall, elBall2, temBallSize, temBall2Size) {
+
+    if (!gIsBallThreeClicked) {
+        elBall.innerText = temBall2Size
+        elBall2.innerText = temBallSize
+    } else {
+        elBall.innerText = temBallSize
+        elBall2.innerText = temBall2Size
+    }
+}
+
+function switchSize(elBall, elBall2, temBallSize, temBall2Size) {
+    if (!gIsBallThreeClicked) {
+        elBall.style.width = temBall2Size + 'px'
+        elBall.style.height = temBall2Size + 'px'
+
+        elBall2.style.width = temBallSize + 'px'
+        elBall2.style.height = temBallSize + 'px'
+    } else {
+        elBall.style.width = temBallSize + 'px'
+        elBall.style.height = temBallSize + 'px'
+
+        elBall2.style.width = temBall2Size + 'px'
+        elBall2.style.height = temBall2Size + 'px'
+    }
+}
+
+function switchColor(elBall, elBall2) {
+    if (!gIsBallThreeClicked) {
+        elBall.style.backgroundColor = gCurrColorBall2
+        elBall2.style.backgroundColor = gCurrColorBall
+
+    } else {
+        elBall.style.backgroundColor = gCurrColorBall
+        elBall2.style.backgroundColor = gCurrColorBall2
+    }
+}
